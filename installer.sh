29101f9106a16e083cf240e9e9fa481d22bc4742
Jason Biatek <jbiatek@gmail.com>
2015-09-30 20:08:27 -0500

    Switched to a stow-style instead of having to maintain an install script

--- installer.sh
+++ installer.sh
@@ -1,38 +1,8 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
-
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="hgrc zshrc zshenv vimrc gvimrc vim gitconfig gitignore_global inputrc oh-my-zsh"    # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-echo "Blindly moving any existing dotfiles from ~ to $olddir,"
-echo "without actually checking to see if they're there."
-echo "If a file isn't found, just ignore it."
-
-for file in $files; do
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
-
+echo "Grabbing oh-my-zsh and other submodules..."
+git submodule init
+git submodule update
 
 echo "Creating local-only shell files..."
 if [ ! -f ~/.zshenv_local ] 
@@ -44,8 +14,6 @@ then
 	echo "# .zshrc is sourced for INTERACTIVE shells." > ~/.zshrc_local
 fi
 
-echo "Now you need to run:"
-echo "    git submodule init"
-echo "    git submodule update"
+echo "To change your shell, run:"
 echo "    chsh -s /bin/zsh"
 echo "That should do it!"

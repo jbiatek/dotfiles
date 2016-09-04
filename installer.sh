f5e8380d18fce3494ea6939708a41fda7b2f2e3e
Jason Biatek <jbiatek@gmail.com>
2016-09-04 14:14:32 -0500

    Reminder to self to use stow

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@ echo "Grabbing oh-my-zsh and other submodules..."
 git submodule init
 git submodule update
 
-echo "Creating local-only shell files..."
+echo "Creating local-only shell files if not already present..."
 if [ ! -f ~/.zshenv_local ] 
 then 
 	echo "# .zshenv is sourced for ALL shells that don't have -f set." > ~/.zshenv_local
@@ -16,4 +16,7 @@ fi
 
 echo "To change your shell, run:"
 echo "    chsh -s /bin/zsh"
+echo "This is now a 'stow' package, so make sure it's in ~/stow or something"
+echo "and then run"
+echo "    stow dotfiles"
 echo "That should do it!"
